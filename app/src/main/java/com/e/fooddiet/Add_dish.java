bf612813b6fa5926024e7234ba8705516c75f7c1
rimza19 <rimzrafeek19@gmail.com>
2020-08-17 09:53:04 +0530

    right and ignore buttons updated

--- app/src/main/java/com/e/fooddiet/Add_dish.java
+++ app/src/main/java/com/e/fooddiet/Add_dish.java
@@ -2,13 +2,38 @@ package com.e.fooddiet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Add_dish extends AppCompatActivity {
 
+    ImageButton okBtn , clBtn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dish);
+
+        okBtn = findViewById(R.id.pick_dish_OK_button);
+        clBtn = findViewById(R.id.pick_dish_cancel_button);
+
+        //creating intent
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(com.e.fooddiet.Add_dish.this,FoodHome.class);
+                startActivity(i);
+            }
+        });
+
+        //creating intent for cancel button
+        clBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(com.e.fooddiet.Add_dish.this,FoodHome.class);
+                startActivity(i1);
+            }
+        });
     }
 }
